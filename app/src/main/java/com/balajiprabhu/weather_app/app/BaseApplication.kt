b4cf64cd96150ac667b654b9e1fc93842bf3616e
balajiprabhu-github <balajiprabhu008@gmail.com>
2021-04-24 16:21:48 +0530

    app shared-preference setup

--- app/src/main/java/com/balajiprabhu/weather_app/app/BaseApplication.kt
+++ app/src/main/java/com/balajiprabhu/weather_app/app/BaseApplication.kt
@@ -1,10 +1,21 @@
-package com.balajiprabhu.weather_app
+package com.balajiprabhu.weather_app.app
 
+import androidx.appcompat.app.AppCompatDelegate
 import com.balajiprabhu.weather_app.di.app.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class BaseApplication : DaggerApplication(){
+
+    override fun onCreate() {
+        super.onCreate()
+        AppPreferences.init(this)
+
+        if (AppPreferences.isNightModeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+    }
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
     }
--- app/src/main/java/com/balajiprabhu/weather_app/di/app/ActivityBuilderModule.kt
+++ app/src/main/java/com/balajiprabhu/weather_app/di/app/ActivityBuilderModule.kt
@@ -1,6 +1,7 @@
 package com.balajiprabhu.weather_app.di.app
 
 import com.balajiprabhu.weather_app.di.home.HomeModule
+import com.balajiprabhu.weather_app.di.home.HomeScope
 import com.balajiprabhu.weather_app.di.home.HomeViewModelModule
 import com.balajiprabhu.weather_app.view.home.HomeActivity
 import dagger.Module
@@ -9,6 +10,7 @@ import dagger.android.ContributesAndroidInjector
 @Module
 abstract class ActivityBuilderModule {
 
+    @HomeScope
     @ContributesAndroidInjector(modules = [HomeViewModelModule::class,HomeModule::class])
     abstract fun contributesHomeActivity(): HomeActivity
 
